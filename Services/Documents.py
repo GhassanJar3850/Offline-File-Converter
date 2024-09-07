1ad63e5abefb88de06ae02baed22253c5480a7bb
Aamir-kanhoush <aamir.kanhoush2001@gmail.com>
2024-09-07 22:07:38 +0300

    Fixed Path and Name problems

--- Services/Documents.py
+++ Services/Documents.py
@@ -5,7 +5,7 @@ import os
 def convert_pdf(input_file, output_format, output_dir=None):
     cv = Converter(input_file)
     filename = os.path.basename(input_file)
-    output_path_and_name = output_dir + "/" + filename + "." + output_format
-    print(output_path_and_name)
-    cv.convert(output_dir + filename+"."+output_format)
+    file_name_without_ext, _ = os.path.splitext(filename)
+    output_path_and_name = os.path.join(output_dir, f"{file_name_without_ext}.{output_format}")
+    cv.convert(output_path_and_name)
     cv.close()
